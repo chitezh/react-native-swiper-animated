c3a44145371bb56ebdd3fe42a418d0b37f7e1174
Kingsley Ochu <kochu@c2gconsulting.com>
2017-02-03 22:53:47 +0100

    Improve basic example ui

--- examples/components/Basic/index.js
+++ examples/components/Basic/index.js
@@ -5,39 +5,42 @@ import {
 } from 'react-native';
 import Swiper from 'react-native-swiper-animated';
 
-  const styles = {
-    wrapper: {
-    },
-    slide1: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#9DD6EB',
-    },
-    slide2: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#97CAE5',
-    },
-    slide3: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#92BBD9',
-    },
-    text: {
-      color: '#fff',
-      fontSize: 30,
-      fontWeight: 'bold',
-    },
-  };
+const styles = {
+  wrapper: {
+    backgroundColor: "#009688"
+  },
+  slide1: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#e91e63',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#673ab7',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3f51b5',
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+};
 
 export default () => <Swiper
   style={styles.wrapper}
   paginationStyle={{ container: { backgroundColor: 'transparent' } }}
-  paginationLeft="home"
-  showsButtons
+  paginationLeft={''}
+  paginationRight={''}
+  smoothTransition
+  loop
 >
   <View style={styles.slide1}>
     <Text style={styles.text}>Hello Swiper</Text>
